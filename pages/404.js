7626cfa0de6b54e8d819532ad46c8a79e38df48c
iamjoydev <iamjoydevsastri@gmail.com>
2025-09-10 12:56:41 +0530

    Create 404.js

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800">
+      <h1 className="text-6xl font-bold">404</h1>
+      <p className="text-lg mt-4">দুঃখিত! আপনি যে পৃষ্ঠাটি খুঁজছেন সেটি পাওয়া যায়নি।</p>
+    </div>
+  );
+}
