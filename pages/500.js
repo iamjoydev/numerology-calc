f7d02ddcda4d4b42e85ad4930c9bf251ba9fc71f
iamjoydev <iamjoydevsastri@gmail.com>
2025-09-10 12:57:13 +0530

    Create 500.js

---
+++ pages/500.js
@@ -0,0 +1,8 @@
+export default function Custom500() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-gray-100 text-gray-800">
+      <h1 className="text-6xl font-bold">500</h1>
+      <p className="text-lg mt-4">সার্ভারে কিছু সমস্যা হয়েছে। পরে আবার চেষ্টা করুন।</p>
+    </div>
+  );
+}
